4d434aa92dd0e26b864bbd08aa7d8069b1624c63
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-08-12 20:29:34 -0300

    feat: chance configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
+import sveltePreprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+const dev = process.argv.includes('dev');
 
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+export default {
+  preprocess: sveltePreprocess(),
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html'
+    }),
+    paths: {
+      base: dev ? '' : '/character-counter'
+    },
+    prerender: {
+      entries: []
+    }
+  }
 };
-
-export default config;
